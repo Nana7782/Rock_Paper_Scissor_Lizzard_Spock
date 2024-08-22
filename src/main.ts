8b5f52f9e3976eb375290e8be208457bf6d85a0b
Nana Leven <nana.leven@mail.de>
2024-08-23 00:17:37 +0200

    the big bang
    Sheldon Cooper Edition of Rock-Paper-Scissor in Typescript
    
    css to be edited 😄

---
+++ src/main.ts
@@ -0,0 +1,82 @@
+const MOVES = ["Rock", "Scissors", "Paper", "Spock", "Lizard"];
+const winningCombinations: { [key: string]: string[] } = {
+  rock: ["Scissors", "Lizard"],
+  scissors: ["Paper", "Lizard"],
+  paper: ["Rock", "Spock"],
+  spock: ["Scissors", "Rock"],
+  lizard: ["Paper", "Spock"],
+};
+
+let totalRounds: number;
+let currentRound: number = 0;
+let playerScore: number = 0;
+let computerScore: number = 0;
+
+document.getElementById("startGame")?.addEventListener("click", () => {
+  const selectedRounds = document.querySelector(
+    'input[name="rounds"]:checked'
+  ) as HTMLInputElement;
+  totalRounds = parseInt(selectedRounds.value);
+  currentRound = 0;
+  playerScore = 0;
+  computerScore = 0;
+
+  document.getElementById("game")!.style.display = "block";
+  document.getElementById("result")!.style.display = "none";
+  document.getElementById("resultMessage")!.innerText = "";
+  updateDisplay();
+});
+
+document.querySelectorAll(".move").forEach((button) => {
+  button.addEventListener("click", (event) => {
+    if (currentRound < totalRounds) {
+      const playerMove = (event.target as HTMLButtonElement).id;
+      const computerMove = MOVES[Math.floor(Math.random() * MOVES.length)];
+      determineWinner(playerMove, computerMove);
+      currentRound++;
+      updateDisplay();
+    }
+  });
+});
+
+function determineWinner(playerMove: string, computerMove: string) {
+  document.getElementById("playerChoice")!.innerText = playerMove;
+  document.getElementById("computerChoice")!.innerText = computerMove;
+
+  let message = "";
+
+  if (playerMove === computerMove) {
+    message = "Draw!";
+  } else if (winningCombinations[playerMove].includes(computerMove)) {
+    playerScore++;
+    message = `${playerMove} beats ${computerMove}!`;
+  } else {
+    computerScore++;
+    message = `${computerMove} beats ${playerMove}!`;
+  }
+
+  document.getElementById("resultMessage")!.innerText = message;
+}
+
+function updateDisplay() {
+  document.getElementById("currentRound")!.innerText = currentRound.toString();
+  document.getElementById("playerScore")!.innerText = playerScore.toString();
+  document.getElementById("computerScore")!.innerText =
+    computerScore.toString();
+
+  if (currentRound === totalRounds) {
+    endGame();
+  }
+}
+
+function endGame() {
+  let resultText = "Unentschieden!";
+  if (playerScore > computerScore) {
+    resultText = "You've won!";
+  } else if (playerScore < computerScore) {
+    resultText = "Computer has won!";
+  }
+
+  document.getElementById("result")!.innerText = resultText;
+  document.getElementById("result")!.style.display = "block";
+}
